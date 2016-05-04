0b5ceb9b0172ad9ab87189302f371adb2a8a48ab
shashi8629 <meshashi29@gmail.com>
2016-05-04 16:10:43 -0700

    Added files via upload
    Consistent hasing algo

--- FileInterface.java
+++ FileInterface.java
@@ -1,30 +1,11 @@
 
+
+
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Hashtable;
 
 public interface FileInterface extends Remote {
-	
-  public String downloadFile(String fileName) throws
+  public byte[] downloadFile(String fileName) throws
   RemoteException;
-  
-  public  String  deleteFile(String fileName) throws
-  RemoteException;
-  
-  
-  public  String   receiveFile(String fileName) throws
-  RemoteException;
-  
-  
-   public String addServer(String hostName)throws
-   RemoteException;
-  
-   
-   public String  deleteServer(String hostName) throws
-   RemoteException;
-  
-   public  Hashtable<String ,Integer>  heartBeatChecking( )  throws
-   RemoteException;
-  
-  
 }
--- FileServer.java
+++ FileServer.java
@@ -1,25 +1,30 @@
 
 
-
 import java.io.*;
 import java.rmi.*;
-import java.rmi.registry.LocateRegistry;
 
 public class FileServer {
    public static void main(String argv[]) {
+	    String homeDir= System.getenv("home.dir");
 	    
-	  
+
+	      
       if(System.getSecurityManager() == null) {
         System.setSecurityManager(new RMISecurityManager());
        // System.setProperty("java.security.policy","/User/shashi/"+"projectPolicyFile");
       }
       
+  
+
+      
      // String homeDir= System.getenv("home.dir");
       //System.setProperty("java.security.policy",homeDir+"/"+"projectPolicyFile");
-     
+      
+      
+      
       try {
-    	  LocateRegistry.createRegistry(1099);
          FileInterface fi = new FileImpl("FileServer");
+         
          Naming.rebind("//127.0.0.1/FileServer", fi);
       } catch(Exception e) {
          System.out.println("FileServer: "+e.getMessage());
